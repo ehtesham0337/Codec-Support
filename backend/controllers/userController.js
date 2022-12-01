330c75ab61ba84c0c374d2210717a4462a4d9545
Syed Ehtesham <ehtesham0337@gmail.com>
2022-12-01 19:55:18 +0500

    register user API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModels')
+const { rmSync } = require('fs')
 
 // @desc    Register User
 // @route   /api/users
@@ -12,7 +16,35 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('Please include all fields')
     }
 
-    res.send('Register Route')
+    // Find if user already exists
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        res.status(400 )
+        throw new Error('User already exists')
+    }
+
+    // Hash passwords
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await User.create({
+        name, 
+        email,
+        password: hashedPassword 
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc    Login User
@@ -21,6 +53,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Route')
 })
+
 
 module.exports = {
     registerUser,
