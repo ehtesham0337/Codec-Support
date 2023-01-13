0268293d8e6815265e0e002bfda4d2e972865f7f
Syed Ehtesham <ehtesham0337@gmail.com>
2023-01-14 00:15:53 +0500

    backend update and delete ticket done

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+} = require("../controllers/ticketController");
 const { protect } = require("../Middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
