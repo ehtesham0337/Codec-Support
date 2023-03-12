817c55143f35319090d9c80823c4f37e7a81313a
Syed Ehtesham <ehtesham0337@gmail.com>
2023-03-12 18:37:00 +0500

    CORS enabled

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,14 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
