99997f7c564cb6b1f0bba5f738e3b6a276f23655
Syed Ehtesham <ehtesham0337@gmail.com>
2023-03-12 16:16:51 +0500

    serve frontend from backend server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to CodecSupport");
+  res.status(200).send("Welcome to CodecSupport API");
 });
 
 // Routes
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
   );
 } else {
   app.get("/", (req, res) => {
-    res.status(200).send("Welcome to CodecSupport");
+    res.status(200).send("Welcome to CodecSupport API");
   });
 }
 
