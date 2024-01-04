99b5b06eaca234c50c8a48ad1bf764c02dc867ee
Syed Ehtesham <ehtesham0337@gmail.com>
2024-01-04 09:18:12 +0500

    mongo config change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 
+const uri = process.env.MONGO_URI;
+if (typeof uri !== 'string') {
+    throw new Error('MONGO_URI must be a string');
+}
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
     
     } catch (error) {
